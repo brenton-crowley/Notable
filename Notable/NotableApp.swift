22f662ca31c81f6c20ce6369f6110ea33b566883
brenton-crowley <brenton.crowley@gmail.com>
2023-03-24 10:08:56 +1100

    Initial Commit

---
+++ Notable/NotableApp.swift
@@ -0,0 +1,20 @@
+//
+//  NotableApp.swift
+//  Notable
+//
+//  Created by Brent Crowley on 24/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotableApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
