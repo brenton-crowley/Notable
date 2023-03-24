2ab09b77223db3be1aea0f98f28cdb4a33396f3f
brenton-crowley <brenton.crowley@gmail.com>
2023-03-25 09:06:19 +1100

    add UI test for successful login

--- NotableUITests/NotableUITests.swift
+++ NotableUITests/NotableUITests.swift
@@ -54,17 +54,30 @@ final class NotableUITests: XCTestCase {
     }
     //Scenario: Login successful
     func testSuccessfulLogin() throws {
-        //Given I type my username in the "Username" field
-        //And password in the "Password" field
-        app.textFields.element.typeText("mike_")
-        app.secureTextFields.element.typeText("20Mike")
-                
-        //When I press the login button
         
+        
+        //Given I type my username in the "Username" field
+        let collectionViewsQuery = app.collectionViews
+        let textView = collectionViewsQuery.children(matching: .cell).element(boundBy: 1).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
+        textView.tap()
+        textView.typeText("mike_")
+        
+        let secureTextField = app/*@START_MENU_TOKEN@*/.collectionViews.containing(.other, identifier:"Vertical scroll bar, 1 page")/*[[".collectionViews.containing(.other, identifier:\"Horizontal scroll bar, 1 page\")",".collectionViews.containing(.other, identifier:\"Vertical scroll bar, 1 page\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.children(matching: .cell).element(boundBy: 3).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .secureTextField).element
+
+        //And password in the "Password" field
+        secureTextField.tap()
+        secureTextField.typeText("20Mike")
+        
+        //When I press the login button
+        app.navigationBars["User Login"]/*@START_MENU_TOKEN@*/.buttons["Login"]/*[[".otherElements[\"Login\"].buttons[\"Login\"]",".buttons[\"Login\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let elementsQuery = app.alerts["Login Successful"].scrollViews.otherElements
+        let successTitle = elementsQuery.staticTexts["Login Successful"]
         
         //Then I should see an alert "Login Successful"
+        XCTAssertTrue(successTitle.exists)
         
-
+        
     }
     
     //Scenario: Home screen after successful login
